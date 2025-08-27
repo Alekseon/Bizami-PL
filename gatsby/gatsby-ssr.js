bcb43bf5ba1793e88046c001ce44f10465ca8ec3
Aleksandra Sobańska <a.sobanska@alekseon.com>
2025-08-27 15:36:38 +0200

    add missing coma

--- gatsby/gatsby-ssr.js
+++ gatsby/gatsby-ssr.js
@@ -21,14 +21,25 @@ export function onRenderBody({ setHeadComponents }) {
     },
     "telephone": "+48 603 603 285",
     "email": "kontakt@bizami.pl",
-    "openingHoursSpecification": [
-      {
-        "@type": "OpeningHoursSpecification",
-        "dayOfWeek": ["Monday","Tuesday","Wednesday","Thursday","Friday"],
-        "opens": "09:00",
-        "closes": "17:00"
-      }
-    ],
+    "location": {
+      "@type": "Place",
+      "address": {
+        "@type": "PostalAddress",
+        "streetAddress": "ul. Kostki Napierskiego 7",
+        "addressLocality": "Gdynia",
+        "postalCode": "81-469",
+        "addressCountry": "PL"
+      },
+      "openingHoursSpecification": [
+        {
+          "@type": "OpeningHoursSpecification",
+          "dayOfWeek": ["Monday","Tuesday","Wednesday","Thursday","Friday"],
+          "opens": "09:00",
+          "closes": "17:00"
+        }
+      ]
+    },
+
     "sameAs": ["https://www.linkedin.com/company/bizami-pl"]
   };
 
